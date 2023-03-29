98dfcd05d1edc007283aa086a5bb5105b4cfd2ac
Nyukeit <nyukeit@outlook.com>
2023-03-29 15:32:26 +0200

    Fixed broken docs link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             items: [
               {
                 label: 'Wiki',
-                to: '/docs/',
+                to: '/',
               },
               {
                 label: 'Blog',
